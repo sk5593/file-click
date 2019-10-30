f0992bdb27e600a21d7941d26671faef37ae67d7
wenbing <wenbing@yeelight.com>
2019-10-30 16:54:41 +0800

    分享

--- src/pages/e_tailors_festival/views/index.vue
+++ src/pages/e_tailors_festival/views/index.vue
@@ -195,7 +195,7 @@
 <script>
     import { getQueryString, validatenull } from '@/util/util';
     import { getToken2, setToken2 } from '@/util/auth'
-    import { getteam, jointeam, opencoupon, config, autoCookie, defaultCoupon } from '@/service/e_tailors_festival';
+    import { getteam, jointeam, opencoupon, config, autoCookie, share, defaultCoupon } from '@/service/e_tailors_festival';
     export default {
         data(){
             return {
@@ -303,25 +303,48 @@
                         } else {
                             this.state = 4;
                         }
-                        // this.initMessage();
+                        this.initMessage();
                     }
                 }, err => {
                     alert(err.data.msg);
                 });
             },
-            // initMessage() {
-            //     try{
-            //         // eslint-disable-next-line no-undef
-            //         wx.miniProgram.postMessage({
-            //             data: {
-            //                 teamId: this.teamId
-            //             }
-            //         });
-            //     }catch(e){
-            //         // eslint-disable-next-line no-console
-            //         console.log(e);
-            //     }
-            // },
+            initMessage() {
+                share(location.href).then(res => {
+                    let json = res.data;
+                    wx.config({
+                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                        appId: json.appId, // 必填，公众号的唯一标识
+                        timestamp:json.timestamp , // 必填，生成签名的时间戳
+                        nonceStr: json.nonceStr, // 必填，生成签名的随机串
+                        signature: json.signature,// 必填，签名
+                        jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData'] // 必填，需要使用的JS接口列表
+                    });
+                    wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
+                        // 自定义“分享给朋友”及“分享到QQ”按钮的分享内容
+                        wx.updateAppMessageShareData({ 
+                            title: 'Yeelight', // 分享标题
+                            desc: '五人携手走，11.11不剁手', // 分享描述
+                            link: 'https://www.baidu.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                            imgUrl: 'https://wx.qlogo.cn/mmhead/Q3auHgzwzM7icwVGibr1W4z2ZcsJTLnvnzOSMJ4sAm7DlMdAuXqAqt1A/64', // 分享图标
+                            success: function () {
+                            // 设置成功
+                                console.log('朋友分享成功')
+                            }
+                        });
+                        // 自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容（
+                        wx.updateTimelineShareData({ 
+                            title: 'Yeelight', // 分享标题
+                            link: 'https://www.baidu.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                            imgUrl: 'https://wx.qlogo.cn/mmhead/Q3auHgzwzM7icwVGibr1W4z2ZcsJTLnvnzOSMJ4sAm7DlMdAuXqAqt1A/64', // 分享图标
+                            success: function () {
+                            // 设置成功
+                                console.log('朋友圈分享成功')
+                            }
+                        })
+                    });
+                })
+            },
             handleBth(){
                 
                 if(this.config.isOpenDateEnd==false) {

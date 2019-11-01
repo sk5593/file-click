6ac453669442d34e10ef88df7497912c0c41bbc9
wenbing <wenbing@yeelight.com>
2019-11-01 18:10:43 +0800

    分享小图标

--- src/pages/e_tailors_festival/views/index.vue
+++ src/pages/e_tailors_festival/views/index.vue
@@ -336,13 +336,12 @@
                 }
                 const shareObj = {
                     link: urlStr,
-                    imgUrl: location.origin + '/img/e_tailors_festival/share_icon.png',
+                    imgUrl: 'https://page.yeelight.com/img/e_tailors_festival/share_icon.jpg',
                 };
-                console.log(shareObj)
                 share(shareObj.link).then(res => {
                     let json = res.data;
                     wx.config({
-                        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+                        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
                         appId: json.appId, // 必填，公众号的唯一标识
                         timestamp:json.timestamp , // 必填，生成签名的时间戳
                         nonceStr: json.nonceStr, // 必填，生成签名的随机串
